a8b3949c382b2d8359fd28eed755b1f4f8dd0496
Nemil Dalal <nemild@gmail.com>
2019-05-18 15:21:28 -0400

    Added header basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './modules/header/Header';
 import Home from './modules/home/Home';
 import Portfolio from './modules/portfolio/Portfolio';
 
 function App() {
   return (
     <div className="App">
+        <Header hideName={false} />
         <Route exact path='/' component={Home}/>
         <Route path='/porfolio' component={Portfolio}/>
     </div>
---
+++ src/modules/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header(props) {
+    return(
+        <div className="logo logo-header">defisurance</div>
+    );
+}
+
+export default Header;
